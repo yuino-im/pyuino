a62c60dd16ce76923929064f3ccdef48f3a1e33b
Masahiko Hashimoto <tsukimi@shilkatsuki.cafe>
2026-02-23 07:20:21 +0000

    Enhance EOF handling and improve YuinoConverter prediction with caching for efficiency

--- pyuino/converter.py
+++ pyuino/converter.py
@@ -1,6 +1,5 @@
 import time
 import torch
-from typing import List
 from .model import YuinoModel
 from .dictionary import YuinoDictionary
 
@@ -13,33 +12,37 @@ class YuinoConverter:
         self._device = device
 
     def convert(self, text):
+        past_key_values = None
         start_time = time.time()
         word_tree = self._dict.build_word_tree(text)
 
-        candidates = []
-        for i, yomi_s in enumerate(word_tree):
-            if i == 0:
-                # Since [CLS] is included, the 0th one is ignored.
-                candidates.append((0., [self._dict.bos_id]))
-                continue
+        with torch.no_grad():
+            candidates = []
+            for i, yomi_s in enumerate(word_tree):
+                if i == 0:
+                    # Since [CLS] is included, the 0th one is ignored.
+                    candidates.append((0., [self._dict.bos_id], past_key_values),)
+                    continue
 
-            min_cost = None
-            min_words = None
-            for yomi in yomi_s:
-                # Predict the next word vector from the previous words
-                pre_words = candidates[i - len(yomi)]
-                pred = self.predict(pre_words[1])
+                min_cost = None
+                min_words = None
+                min_past_key_values = None
+                for yomi in yomi_s:
+                    # Predict the next word vector from the previous words
+                    pre_words = candidates[i - len(yomi)]
+                    pred, past_key_values = self.predict(pre_words[1][-1], pre_words[2])
 
-                for wid in self._dict.gets(yomi):
-                    embed = self._dict.embed([wid]).squeeze(0)
-                    cost = self.loss(pred, embed) + pre_words[0]
-                    if min_cost is None or cost < min_cost:
-                        min_cost = cost
-                        min_words = pre_words[1] + [wid]
+                    for wid in self._dict.gets(yomi):
+                        embed = self._dict.embed([wid]).squeeze(0)
+                        cost = self.loss(pred, embed) + pre_words[0]
+                        if min_cost is None or cost < min_cost:
+                            min_cost = cost
+                            min_words = pre_words[1] + [wid]
+                            min_past_key_values = past_key_values
 
-            # fixed this index
-            candidates.append((min_cost, min_words))
-            print(min_cost, [self._dict.surface(wid) for wid in min_words])
+                # fixed this index
+                candidates.append((min_cost, min_words, min_past_key_values))
+                print(min_cost, [self._dict.surface(wid) for wid in min_words])
 
         fixed_words = ""
         for i, word in enumerate(candidates[-1][1]):
@@ -49,11 +52,10 @@ class YuinoConverter:
         print("%s : %f sec" % (fixed_words, time.time() - start_time))
         return fixed_words
 
-    def predict(self, words: List[int]):
-        wt = self._dict.embed(words).to(self._device)
-        with torch.no_grad():
-            y = self._model(inputs_embeds=wt)
-        return y.logits[:, -1, :]
+    def predict(self, wid: int, past_key_values):
+        wt = self._dict.embed([wid]).to(self._device)
+        y = self._model(inputs_embeds=wt, past_key_values=past_key_values, use_cache=True)
+        return y.logits[:, -1, :], y.past_key_values
 
     def loss(self, y, y_hat):
         return self._loss_func(y, y_hat).item()
